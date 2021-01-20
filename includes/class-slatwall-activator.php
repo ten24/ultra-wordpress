2e2868b1739375576911fca4680154b3047e20a4
Brad <brad.gustavesen@ten24web.com>
2021-01-20 16:55:00 -0500

    more cleanup

--- includes/class-slatwall-activator.php
+++ includes/class-slatwall-activator.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * Copyright © ten24, LLC Inc. All rights reserved.
+ * See License.txt for license details.
+ */
 
 /**
  * Fired during slatwall activation
@@ -18,7 +22,6 @@
  * @since      1.0.0
  * @package    Slatwall_Ecommerce
  * @subpackage Slatwall_Ecommerce/includes
- * @author     Yash <raj.yash@orangemantra.in>
  */
 class Slatwall_Activator {
 
@@ -30,7 +33,7 @@ class Slatwall_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-          
+
             global $table_prefix, $wpdb;
 
     $tblname = 'slatwall_login';
@@ -38,7 +41,7 @@ class Slatwall_Activator {
     $charset_collate = $wpdb->get_charset_collate();
     #Check to see if the table exists already, if not, then create it
 
-    if($wpdb->get_var( "show tables like '$wp_track_table'" ) != $wp_track_table) 
+    if($wpdb->get_var( "show tables like '$wp_track_table'" ) != $wp_track_table)
     {
 
         $sql = "CREATE TABLE $wp_track_table (
@@ -50,12 +53,12 @@ class Slatwall_Activator {
                 `status`  ENUM('0', '1') DEFAULT '0',
                 UNIQUE KEY id (id)
         ) $charset_collate;";
-        
-        
+
+
         require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
         dbDelta($sql);
     }
-            
+
 	}
 
 }
